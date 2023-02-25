c0697537982854e6d6aff8ef97deebbfd6df21b5
JuodaanViinaa <mkxraujg_kxra@hotmail.com>
2023-02-25 12:16:12 -0600

    Actualización de texto de estadística inferencial y presentaciones. Creación de presentación de percepción 1

--- Bibliografia/MetodologiaYEstadistica/TCM.py
+++ Bibliografia/MetodologiaYEstadistica/TCM.py
@@ -2,17 +2,36 @@ from statistics import mean
 import matplotlib.pyplot as plt
 import random
 
-datos = []
-for i in range(100):
-    datos.append(random.randint(145, 180))
-print(mean(datos))
+# datos = []
+# for i in range(100):
+#     datos.append(random.randint(145, 180))
+# print(mean(datos))
+# 
+# medias = []
+# for _ in range(1000000):
+#     muestra = random.sample(datos, 10)
+#     media = mean(muestra)
+#     medias.append(media)
+# print(mean(medias))
+# 
+# plt.hist(medias, bins=50)
+# plt.show()
+datos = [165, 163, 160, 152, 160, 155, 156, 158, 165, 149, 182, 155, 150, 177, 
+         159, 175, 163, 155, 152, 170, 161, 160, 173]
 
-medias = []
+media_pob = mean(datos)
+print(media_pob)
+
+diferencias = []
 for _ in range(1000000):
-    muestra = random.sample(datos, 10)
-    media = mean(muestra)
-    medias.append(media)
-print(mean(medias))
+    muestra1 = random.sample(datos, 5)
+    muestra2 = random.sample(datos, 5)
+    media1 = mean(muestra1)
+    media2 = mean(muestra2)
+    diferencias.append(media1 - media2)
+diferencia_media = mean(diferencias)
 
-plt.hist(medias, bins=50)
+print(f"Diferencia entre medias: {diferencia_media}")
+
+plt.hist(diferencias, bins=50, range=(-20, 20))
 plt.show()
